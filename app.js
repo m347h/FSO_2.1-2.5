56057f2d2448acf201192a0f2a5ea82ac78ae3ba
M✩H <linjuehan788@gmail.com>
2023-08-09 16:03:34 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,55 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+{parts.name}
+
+}
+export default App
+
+
+
+<div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </div>
+    
+  /*
+  
+  
+  
+   const result = parts.map(parts => parts.id)
+console.log(result)
+
+
+return (
+
+   <p>{name}<p/> 
+  )<Course course={course} />
+
+}
+*/
+ 
